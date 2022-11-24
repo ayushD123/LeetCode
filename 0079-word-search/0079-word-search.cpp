9d1c16514a597d5fe25c1e55d7e1b851b7a5a593
AYUSH DIXIT <72985069+ayushD123@users.noreply.github.com>
2022-11-24 21:31:51 +0530

    Time: 714 ms (49.84%), Space: 8.1 MB (46.96%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -29,8 +29,8 @@ public:
         for(int i=0;i<n;++i){
             for(int j=0;j<m;++j){
                 if(board[i][j]==word[0]){
-                    vector<vector<int>> temp=vis;
-                    if(dfs(i,j,board,word,temp,cnt)){
+                //    vector<vector<int>> temp=vis;
+                    if(dfs(i,j,board,word,vis,cnt)){
                     return true;
                 }
             }
