910853f4ce47ffad2773770ef11bc6fbd1ff6ed2
AYUSH DIXIT <72985069+ayushD123@users.noreply.github.com>
2022-11-28 23:36:34 +0530

    Time: 42 ms (70.95%), Space: 13.7 MB (39.66%) - LeetHub

---
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -0,0 +1,45 @@
+class Solution {
+    private:
+    bool bfs(int node,int color[],int V,vector<vector<int>>& adj){
+        color[node]=0;
+        queue<int> q;
+        q.push(node);
+        while(!q.empty()){
+            int par=q.front();
+            q.pop();
+            for(auto it:adj[par]){
+		    // if the adjacent node is yet not colored
+	            // you will give the opposite color of the node 
+                if(color[it]==-1){
+                    color[it]=!color[par];
+                    q.push(it);
+                }else{
+			// is the adjacent guy having the same color 
+	            // someone did color it on some other path 
+                    if(color[it]==color[par]){
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+public:
+    bool isBipartite(vector<vector<int>>& graph) {
+        int V=graph.size();
+        
+     int color[V];
+	    for(int i=0;i<V;++i){
+	        color[i]=-1;
+	    }
+	    for(int i=0;i<V;++i){
+	        if(color[i]==-1){
+	           if(bfs(i,color,V,graph)==false){
+	               return false;
+	           }
+	        }
+	    }
+	    return true;
+	    
+	}
+};
