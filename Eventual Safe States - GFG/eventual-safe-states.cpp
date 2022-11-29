45d30c61624de091c7e36d88d156fb7efe956354
AYUSH DIXIT <72985069+ayushD123@users.noreply.github.com>
2022-11-29 23:31:19 +0530

    Added solution - LeetHub

--- Eventual Safe States - GFG/eventual-safe-states.cpp
+++ Eventual Safe States - GFG/eventual-safe-states.cpp
@@ -7,53 +7,48 @@ using namespace std;
 
 // } Driver Code Ends
 // User function Template for C++
-
 class Solution {
-    private:
-    bool dfs(int src,vector<int> adj[],int vis[],int path[],int check[]){
-        vis[src]=1;
-        path[src]=1;
-       // check[src]=0;
-        for(auto node:adj[src]){
-            if(!vis[node]){
-               if (dfs(node,adj,vis,path,check)){
-                  // check[src]=0;
-                 return true;
-             }
-            }else{
-                if(path[node]==1){
-                   // check[src]=0;
-                    return true;
-                }
-            }
-            //path[node]=0;
-        }
-        check[src]=1;
-        path[src]=0;
-        return false;
-        //.push_back(src);
-        
-    }
   public:
+  bool checkdfs(int node,vector<int> adj[],int vis[],int pathvis[],vector<int> &temp){
+      vis[node] = 1;
+      pathvis[node] = 1;
+      temp.push_back(node);
+      for(auto neigh : adj[node]){
+          if(!vis[neigh]){
+              if(checkdfs(neigh,adj,vis,pathvis,temp)){
+                 //we are not back tracking the path array
+                  return true;
+              }
+          }
+          else if(pathvis[neigh]){
+               //we are not back tracking the path array
+
+              return true;
+          }
+      }
+     
+      // the cycle is not presant back track the path array
+      pathvis[node] = 0;
+      return false;
+  }
     vector<int> eventualSafeNodes(int V, vector<int> adj[]) {
         // code here
+        int vis[V] ={0};
+        int pathvis[V] = {0};
+        set<int >s;
         vector<int> ans;
-        int vis[V]={0};
-        int check[V]={0};
-        int path[V]={0};
-        for(int i=0;i<V;++i){
-            if(!vis[i]){
-               dfs(i,adj,vis,path,check);
-                   
-               
-            }
+        for(int i=0;i<V;i++){
+            vector<int> temp;
+            
+                if(checkdfs(i,adj,vis,pathvis,temp)){
+                    continue;
+                }
+                else{
+                    ans.push_back(i);
+                }
             
         }
-        for(int i=0;i<V;++i){
-            if(check[i]){
-                ans.push_back(i);
-            }
-        }
+       
         return ans;
     }
 };
