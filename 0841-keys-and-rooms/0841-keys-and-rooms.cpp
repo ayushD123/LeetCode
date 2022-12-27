18e817c1ec615648f3110cb20e0904a6bf466d06
AYUSH DIXIT <72985069+ayushD123@users.noreply.github.com>
2022-12-28 00:38:53 +0530

    Time: 22 ms (35.00%), Space: 11.3 MB (7.88%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.cpp
+++ 0841-keys-and-rooms/0841-keys-and-rooms.cpp
@@ -1,17 +1,29 @@
 class Solution {
 public:
-     bool canVisitAllRooms(vector<vector<int>>& rooms) {
-        stack<int> dfs; dfs.push(0);
-        unordered_set<int> seen = {0};
-        while (!dfs.empty()) {
-            int i = dfs.top(); dfs.pop();
-            for (int j : rooms[i])
-                if (seen.count(j) == 0) {
-                    dfs.push(j);
-                    seen.insert(j);
-                    if (rooms.size() == seen.size()) return true;
-                }
+    void dfs(int node,vector<int> &vis,vector<int> adj[]){
+        vis[node]=1;
+        for(auto it:adj[node]){
+            if(vis[it]==0){
+                dfs(it,vis,adj);
+            }
         }
-        return rooms.size() == seen.size();
+    }
+    
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        vector<int> adj[rooms.size()];
+        for(int i=0;i<rooms.size();++i){
+            for(int j=0;j<rooms[i].size();++j){
+                adj[i].push_back(rooms[i][j]);
+            }
+        }
+        vector<int> vis(rooms.size(),0);
+        dfs(0,vis,adj);
+        for(auto it:vis){
+            cout<<it<<" ";
+            if(it==0){
+                return false;
+            }
+        }
+        return true;
     }
 };
