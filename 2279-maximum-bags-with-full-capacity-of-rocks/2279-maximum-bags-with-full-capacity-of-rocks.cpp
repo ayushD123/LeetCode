cd647235ca61927073f9cdf2751e0d9bfb795685
AYUSH DIXIT <72985069+ayushD123@users.noreply.github.com>
2022-12-27 21:04:42 +0530

    Time: 546 ms (21.53%), Space: 87.3 MB (55.82%) - LeetHub

---
+++ 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+   int maximumBags(vector<int>& capacity, vector<int>& rocks, int additionalRocks) {
+        long long totalcap=0;
+        for(auto it:capacity){
+            totalcap+=it;
+        }
+        long long totalrocks=0;
+         for(auto it:rocks){
+            totalrocks+=it;
+        }
+        if(totalrocks+additionalRocks>=totalcap){
+            return rocks.size();
+        }
+        int cnt=0;
+        int n=rocks.size();
+        vector<int> rem;
+        for(int i=0;i<rocks.size();++i){
+            rem.push_back(capacity[i]-rocks[i]);
+        }
+
+        sort(rem.begin(),rem.end());
+        for(auto it:rem){
+            cout<<it<<" ";
+        }
+        cout<<endl;
+        int sum=0;
+        int i=0;
+        while(sum<=additionalRocks && i<n){
+            sum+=rem[i];
+            rem[i]=0;
+            i++;
+        }
+       //  int i=0;
+       // int k= additionalRocks;
+       //  while(k>0){
+       //      if(rem[i%n]>0){
+       //          rem[i%n]--;
+       //          k--;
+                
+       //      }
+       //      i++;
+       //  }
+        for(auto it:rem){
+            cout<<it<<" ";
+        }
+        cout<<endl;
+       // sort(rem.begin(),rem.end());
+        for(auto it:rem){
+            if(it<=0){
+                cnt++;
+            }
+        }
+        return cnt-1;
+        
+    }
+};
