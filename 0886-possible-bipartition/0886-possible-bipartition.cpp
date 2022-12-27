7f94def2196891dfff1df3cfdfeba86b3f82c403
AYUSH DIXIT <72985069+ayushD123@users.noreply.github.com>
2022-12-28 01:30:41 +0530

    Time: 194 ms (97.24%), Space: 63.6 MB (95.02%) - LeetHub

--- 0886-possible-bipartition/0886-possible-bipartition.cpp
+++ 0886-possible-bipartition/0886-possible-bipartition.cpp
@@ -1,41 +1,67 @@
 class Solution {
 public:
-    bool bfs(int source, vector<vector<int>>& adj, vector<int>& color) {
+     bool bfs(int node,int color[],vector<int>adj[]){
+        color[node]=0;
         queue<int> q;
-        q.push(source);
-        color[source] = 0;  // Start with marking source as `RED`.
-
-        while (!q.empty()) {
-            int node = q.front();
+        q.push(node);
+        while(!q.empty()){
+            int par=q.front();
             q.pop();
-            for (auto& neighbor : adj[node]) {
-                // If there is a conflict, return false.
-                if (color[neighbor] == color[node]) return false;
-                if (color[neighbor] == -1) {
-                    color[neighbor] = 1 - color[node];
-                    q.push(neighbor);
+            for(auto it:adj[par]){
+                if(color[it]==-1){
+                    color[it]=!color[par];
+                    q.push(it);
+                }else{
+                    if(color[it]==color[par]){
+                        return false;
+                    }
                 }
             }
         }
         return true;
     }
-
-    bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
-        vector<vector<int>> adj(n + 1);
-        for (auto& dislike : dislikes) {
-            adj[dislike[0]].push_back(dislike[1]);
-            adj[dislike[1]].push_back(dislike[0]);
-        }
-
-        vector<int> color(n + 1, -1); // 0 stands for red and 1 stands for blue.
-        for (int i = 1; i <= n; i++) {
-            if (color[i] == -1) {
-                // For each pending component, run BFS.
-                if (!bfs(i, adj, color))
-                    // Return false, if there is conflict in the component.
+    bool dfs(int node,vector<int> adj[],vector<int> &col,int color){
+        col[node]=color;
+        for(auto it:adj[node]){
+            if(col[it]==-1){
+                if(!dfs(it,adj,col,!color)){
                     return false;
+                }
+            }else{
+                if(col[it]==col[node]){
+                    return false;
+                }
             }
         }
         return true;
     }
+    bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
+        if(n<=1){
+            return true;
+        }
+        vector<int> adj[n+1];
+        int color[n+1];
+        // int col[n+1];
+        // for(int i=0;i<=n;++i){
+        //     col[i]=-1;
+        // }
+        for(int i=0;i<dislikes.size();++i){
+        //    for(int j=0;j<2;++j){
+                adj[dislikes[i][0]].push_back(dislikes[i][1]);
+            adj[dislikes[i][1]].push_back(dislikes[i][0]);
+         //   }
+        }
+        for(int i=1;i<=n;++i){
+	        color[i]=-1;
+	    }
+	    for(int i=1;i<=n;++i){
+	        if(color[i]==-1){
+	           if(bfs(i,color,adj)==false){
+	               return false;
+	           }
+	        }
+	    }
+	    return true;
+	    
+	}
 };
