7906832d7b1baaa6c6392f81859a864f76abc124
AYUSH DIXIT <72985069+ayushD123@users.noreply.github.com>
2022-12-28 20:38:15 +0530

    Added solution - LeetHub

--- Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
+++ Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
@@ -4,14 +4,15 @@ using namespace std;
 
 // } Driver Code Ends
 class Solution {
-    
+    //using DFS
+    /*
     private:
     bool dfs(int src,vector<int> adj[],int vis[],int path[]){
         vis[src]=1;
         path[src]=1;
         for(auto node:adj[src]){
             if(!vis[node]){
-               if (dfs(node,adj,vis,path)){
+               if(dfs(node,adj,vis,path)){
                  return true;
              }
             }else{
@@ -21,8 +22,8 @@ class Solution {
             }
             //path[node]=0;
         }
-        path[src]=0;
-        return false;
+        // path[src]=0;
+        // return false;
     }
   public:
     // Function to detect cycle in a directed graph.
@@ -38,6 +39,55 @@ class Solution {
         }
         return false;
     }
+    */
+    //USING BFS(TOPOLOGICAL SORT->KAHN)
+    public:
+    // Function to detect cycle in a directed graph.
+    bool isCyclic(int V, vector<int> adj[]) {
+        vector<int> ans;
+	   vector<int> inDeg(V,0);
+	  for(int i=0;i<V;++i){
+	      for(auto it:adj[i]){
+	          inDeg[it]++;
+	      }
+	  }
+// 	  for(int i=0;i<V;++i){
+// 	      cout<<i<<"->"<<inDeg[i]<<endl;
+// 	  }
+	  
+	   
+	   queue<int> q;
+	 for(int i=0;i<V;++i){
+	     if(inDeg[i]==0){
+	         q.push(i);
+	      ;
+	         ans.push_back(i);
+	     }
+	 }
+	 
+	 while(!q.empty()){
+	     int node=q.front();
+	     q.pop();
+	     for(auto it:adj[node]){
+	      //   cout<<node<<"->"<<it<<endl;
+	     //    if(inDeg[it]>0){
+	            
+	             inDeg[it]--;
+	             if(inDeg[it]==0){
+	                 ans.push_back(it);
+	                 q.push(it);
+	           //  }
+	         }
+	     }
+	     
+	     
+	 }
+	 if(ans.size()!=V){
+	     return 1;
+	 }
+	 return 0;
+    }
+    
 };
 
 //{ Driver Code Starts.
