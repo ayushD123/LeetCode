00548a9407ef95c8bfa63bb5a1d5883222fad109
AYUSH DIXIT <72985069+ayushD123@users.noreply.github.com>
2022-12-28 20:56:36 +0530

    Added solution - LeetHub

---
+++ Prerequisite Tasks - GFG/prerequisite-tasks.cpp
@@ -0,0 +1,82 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+public:
+	bool isPossible(int N, vector<pair<int, int> >& pre) {
+	    // Code here
+	    int V=N;
+	      vector<int> ans;
+	      vector<int> adj[N];
+	      for(auto it:pre){
+	          adj[it.first].push_back(it.second);
+	      }
+	   vector<int> inDeg(V,0);
+	  for(int i=0;i<V;++i){
+	      for(auto it:adj[i]){
+	          inDeg[it]++;
+	      }
+	  }
+
+	  
+	   
+	   queue<int> q;
+	 for(int i=0;i<V;++i){
+	     if(inDeg[i]==0){
+	         q.push(i);
+	      ;
+	         ans.push_back(i);
+	     }
+	 }
+	 
+	 while(!q.empty()){
+	     int node=q.front();
+	     q.pop();
+	     for(auto it:adj[node]){
+	    
+	             inDeg[it]--;
+	             if(inDeg[it]==0){
+	                 ans.push_back(it);
+	                 q.push(it);
+	           //  }
+	         }
+	     }
+	     
+	     
+	 }
+	 if(ans.size()!=V){
+	     return 0;
+	 }
+	 return 1;
+    
+	}
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+    	int N, P;
+        vector<pair<int, int> > prerequisites;
+        cin >> N;
+        cin >> P;
+        for (int i = 0; i < P; ++i) {
+            int x, y;
+            cin >> x >> y;
+            prerequisites.push_back(make_pair(x, y));
+        }
+        // string s;
+        // cin>>s;
+        Solution ob;
+        if (ob.isPossible(N, prerequisites))
+            cout << "Yes";
+        else
+            cout << "No";
+        cout << endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
