ab36e9c6ad43b8290e5db9d9e5c5e976a215bee5
AYUSH DIXIT <72985069+ayushD123@users.noreply.github.com>
2023-01-02 20:22:09 +0530

    Update maximum-value.cpp

--- Maximum Value - GFG/maximum-value.cpp
+++ Maximum Value - GFG/maximum-value.cpp
@@ -12,17 +12,28 @@ struct Node {
 };
 
 
-// } Driver Code Ends
-//User function Template for C++
-/* Tree node structure  used in the program
-
-struct Node
-{
-    int data;
-    struct Node* left;
-    struct Node* right;
-}; */
-
+/*EDITORIAL SOLN
+class Solution {
+  public:
+    void solve(Node* node,int level,vector<int> &answer)
+    {
+        if(node==NULL)
+            return ;
+        if(level==answer.size())
+            answer.push_back(node->data);
+        else
+            answer[level]=max(answer[level],node->data);
+        solve(node->left,level+1,answer);
+        solve(node->right,level+1,answer);
+    }
+    
+    vector<int> maximumValue(Node* node) {
+       vector<int> answer;
+       solve(node,0,answer);
+       return answer;
+    }
+};
+*/
 class Solution {
   public:
   int lvl(Node * root,vector<int> &maxx) {
