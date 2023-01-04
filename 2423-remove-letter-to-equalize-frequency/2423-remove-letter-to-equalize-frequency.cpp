abe58c6cf6e9ef0f1c3980e5c67992498128ec09
AYUSH DIXIT <72985069+ayushD123@users.noreply.github.com>
2023-01-05 00:24:04 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (74.63%) - LeetHub

--- 2423-remove-letter-to-equalize-frequency/2423-remove-letter-to-equalize-frequency.cpp
+++ 2423-remove-letter-to-equalize-frequency/2423-remove-letter-to-equalize-frequency.cpp
@@ -1,26 +1,46 @@
 class Solution {
 public:
     bool equalFrequency(string word) {
-        vector<int> freq(26); // maintain frequency of every character
+//         unordered_map<char, int> mp1;
+//         unordered_map<int, int> mp2;
+//         for(auto it: word){
+//             mp1[it]++;
+           
+//         }  
+//          for(auto it: mp1){
+//             mp2[it.second]++;
+           
+//         }
+//         if(mp2.size()>2) return false;
         
-        for(char ch : word) {
-            freq[ch - 'a'] += 1;
+        
+        
+        vector<int> freq(26);
+        
+        for(auto it : word) {
+            freq[it - 'a'] += 1;
         }
         
-        int mx_freq = 0, mn_freq = word.size(), cnt_mx = 0, cnt_mn = 0;
+        int mx_freq = 0;
+        int mn_freq = word.size();
+        int cnt_mx = 0;
+        int cnt_mn = 0;
         
         for(int i = 0; i < 26; ++i) {
-            mx_freq = max(mx_freq, freq[i]);                  // maintain max frequency
-            if(freq[i] > 0) mn_freq = min(mn_freq, freq[i]);  // maintain min frequency
+            mx_freq = max(mx_freq, freq[i]);                 
+            if(freq[i] > 0) mn_freq = min(mn_freq, freq[i]); 
+        }
+        for(auto it:freq){
+            cout<<it<<" ";
         }
         
         for(int i = 0; i < 26; ++i) {
             if(freq[i] == mx_freq)
-                ++cnt_mx;         // increment count of characters with max frequency
+                ++cnt_mx;        
             else if(freq[i] == mn_freq)
-                ++cnt_mn;        // increment count of characters with min frequency
-            else if(freq[i] > 0) 
-                return false;   // distinct frequencies is greater than 2
+                ++cnt_mn;      
+             else if(freq[i] > 0) 
+                 return false;  
         }
         
         if(mx_freq == mn_freq)
