83d416b995c9a9f0e57dc4b55810dd06a12d6c73
AYUSH DIXIT <72985069+ayushD123@users.noreply.github.com>
2023-01-20 19:04:29 +0530

    Time: 1009 ms (5.09%), Space: 238.3 MB (5.26%) - LeetHub

--- 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
@@ -3,35 +3,43 @@ public:
     void subseq(int ind,set<vector<int>> &ans,vector<int> nums,int n,vector<int> temp){
         if(ind==n){
             vector<int> a;
-            if(temp.size()<2){
-                return;
+            if(temp.size()>=2){
+               ans.insert(temp);
             }
-            for(auto it:temp){
-                a.push_back(it);
-            }
-            ans.insert(a);
+          //  ans.insert(a);
             return;
         }
-        subseq(ind+1,ans,nums,n,temp);
-        if(temp.size()==0){
-            temp.push_back(nums[ind]);
-        }else if(nums[ind]>=temp.back()){
-           temp.push_back(nums[ind]); 
-        }
-        
+         if(temp.size()==0 || temp.back() <= nums[ind]){
+             temp.push_back(nums[ind]);
+          
+       
+         }
          subseq(ind+1,ans,nums,n,temp);
+       
         temp.pop_back();
+         subseq(ind+1,ans,nums,n,temp);
+        
     }
     
     vector<vector<int>> findSubsequences(vector<int>& nums) {
        vector<vector<int>> ans;
         set<vector<int>> st;
+        vector<int> arr;
+        arr.push_back(nums[0]);
+        int max=nums[0];
+        for(int i=1;i<nums.size();++i){
+            if(nums[i]>=max){
+                max=nums[i];
+                arr.push_back(max);
+            }
+        }
         int n=nums.size();
         vector<int> temp;
         subseq(0,st,nums,n,temp);
         for(auto it:st){
             ans.push_back(it);
         }
+      //  ans(st.begin(),st.end());
         return ans;
     }
 };
