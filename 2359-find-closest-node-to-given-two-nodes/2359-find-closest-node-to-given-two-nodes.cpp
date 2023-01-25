11e165cedb97889ea185debf290807f074130a11
AYUSH DIXIT <72985069+ayushD123@users.noreply.github.com>
2023-01-26 00:38:44 +0530

    Time: 188 ms (78.95%), Space: 104.8 MB (52.35%) - LeetHub

--- 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.cpp
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.cpp
@@ -1,45 +1,36 @@
 class Solution {
 public:
-    void bfs(int startNode, vector<int>& edges, vector<int>& dist) {
-        int n = edges.size();
-        queue<int> q;
-        q.push(startNode);
+    void dfs(int node,vector<int> &edges,vector<int> &dist,vector<int> &vis){
 
-        vector<bool> visit(n);
-        dist[startNode] = 0;
-
-        while (!q.empty()) {
-            int node = q.front();
-            q.pop();
-
-            if (visit[node]) {
-                continue;
-            }
-
-            visit[node] = true;
-            int neighbor = edges[node];
-            if (neighbor != -1 && !visit[neighbor]) {
-                dist[neighbor] = 1 + dist[node];
-                q.push(neighbor);
-            }
+        vis[node]=1;
+        int neigh=edges[node];
+        if(neigh!=-1 && vis[neigh]==0){
+            dist[neigh]=dist[node]+1;
+            dfs(neigh,edges,dist,vis);
         }
     }
-
     int closestMeetingNode(vector<int>& edges, int node1, int node2) {
-        int n = edges.size();
-        vector<int> dist1(n, numeric_limits<int>::max()), dist2(n, numeric_limits<int>::max());
-
-        bfs(node1, edges, dist1);
-        bfs(node2, edges, dist2);
-
-        int minDistNode = -1, minDistTillNow = numeric_limits<int>::max();
-        for (int currNode = 0; currNode < n; currNode++) {
-            if (minDistTillNow > max(dist1[currNode], dist2[currNode])) {
-                minDistNode = currNode;
-                minDistTillNow = max(dist1[currNode], dist2[currNode]);
+        int n=edges.size();
+        vector<int> dist1(n,1e5+10);
+        vector<int> dist2(n,1e5+10);
+        vector<int> vis1(n,0);
+        vector<int> vis2(n,0);
+        dist1[node1]=0;
+        dist2[node2]=0;
+        dfs(node1,edges,dist1,vis1);
+        dfs(node2,edges,dist2,vis2);
+       
+        int ans=1e5+10;
+        int anss=-1;
+        for(int i=0;i<n;++i){
+            int a=max(dist1[i],dist2[i]);
+            if(a<ans){
+                ans=a;
+                anss=i;
             }
         }
-
-        return minDistNode;
+      
+      
+        return anss;
     }
 };
