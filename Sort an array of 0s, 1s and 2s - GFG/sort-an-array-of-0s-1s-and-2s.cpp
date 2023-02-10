32f5d4f86d3b251dfda3e708c0dbc76e3c56d1a0
AYUSH DIXIT <72985069+ayushD123@users.noreply.github.com>
2023-02-11 03:13:50 +0530

    Added solution - LeetHub

--- Sort an array of 0s, 1s and 2s - GFG/sort-an-array-of-0s-1s-and-2s.cpp
+++ Sort an array of 0s, 1s and 2s - GFG/sort-an-array-of-0s-1s-and-2s.cpp
@@ -11,23 +11,49 @@ class Solution
     void sort012(int a[], int n)
     {
         // code here 
-        int lo=0;
-        int mid=0;
-        int high=n-1;
-        while(mid<=high){
-            if(a[mid]==0){
-                swap(a[mid],a[lo]);
-                mid++;
-                lo++;
-            }else{
-                 if(a[mid]==2){
-                swap(a[mid],a[high]);
-                //mid++;
-                high--;
-            }else{
-                mid++;
-            }
+    //     int lo=0;
+    //     int mid=0;
+    //     int high=n-1;
+    //     while(mid<=high){
+    //         if(a[mid]==0){
+    //             swap(a[mid],a[lo]);
+    //             mid++;
+    //             lo++;
+    //         }else{
+    //              if(a[mid]==2){
+    //             swap(a[mid],a[high]);
+    //             //mid++;
+    //             high--;
+    //         }else{
+    //             mid++;
+    //         }
+    //     }
+    // }
+    
+    int cnt0=0;
+    int cnt1=0;
+    int cnt2=0;
+    for(int i=0;i<n;++i){
+        if(a[i]==0){
+            cnt0++;
+        }else if(a[i]==1){
+            cnt1++;
+        }else{
+            cnt2++;
         }
+    }
+    int i=0;
+    while(cnt0>0){
+        a[i++]=0;
+        cnt0--;
+    }
+     while(cnt1>0){
+        (a[i++]=1);
+        cnt1--;
+    }
+     while(cnt2>0){
+        (a[i++]=2);
+        cnt2--;
     }
     }
     
