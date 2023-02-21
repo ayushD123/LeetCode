9299e1eb2a9ada321964edd1ba71db99c8cc1b14
AYUSH DIXIT <72985069+ayushD123@users.noreply.github.com>
2023-02-21 23:02:08 +0530

    Time: 31 ms (49.25%), Space: 22.3 MB (62.74%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -4,7 +4,7 @@ public:
        int left=0;
         int right=nums.size()-1;
         while(left<right){
-            int mid=left + (right-left)/2;
+            int mid=(left + right)/2;
             if((mid%2==0 && nums[mid]==nums[mid+1] )|| (mid%2!=0 && nums[mid]==nums[mid-1])){
                left=mid+1;
             }else{
