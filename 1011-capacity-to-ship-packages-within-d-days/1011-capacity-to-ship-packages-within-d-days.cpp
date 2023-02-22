37e3d653943bc1f6c9394394713ba4c6651a7c79
AYUSH DIXIT <72985069+ayushD123@users.noreply.github.com>
2023-02-22 21:32:01 +0530

    Time: 57 ms (74.65%), Space: 26.1 MB (37.41%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -6,9 +6,25 @@ public:
         for(auto it:weights){
             high+=it;
         }
-        int ans=0;
-        for(int cap=low;cap<=high;++cap){
-          int dd=1;
+        int ans=1e5+10;
+        // for(int cap=low;cap<=high;++cap){
+        //   int dd=1;
+        //   int sum=0;
+        //     for(auto it:weights){
+        //         sum+=it;
+        //         if(sum>cap){
+        //             dd++;
+        //             sum=it;
+        //         }
+        //     }
+        //     if(dd<=days){
+        //         ans=cap;
+        //         break;
+        //     }
+        // }
+        while(low<=high){
+            int cap=low + (high-low)/2;
+             int dd=1;
           int sum=0;
             for(auto it:weights){
                 sum+=it;
@@ -18,10 +34,15 @@ public:
                 }
             }
             if(dd<=days){
-                ans=cap;
-                break;
+                high=cap-1;
+                ans=min(ans,cap);
+                
+            }else{
+                low=cap+1;
+              
             }
+            
         }
-        return ans;
+        return low;
     }
 };
