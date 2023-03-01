f7471fb9d36fd2ab069d272e783eaa3f57daa683
AYUSH DIXIT <72985069+ayushD123@users.noreply.github.com>
2023-03-01 23:40:35 +0530

    Time: 255 ms (43.42%), Space: 89.1 MB (31.58%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,7 +1,25 @@
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        
+        unordered_map<int,int> mp;
+        int maxx=-1e9+10;
+        int minn=1e9+10;
+        for(auto it:nums){
+            maxx=max(maxx,it);
+            minn=min(minn,it);
+            mp[it]++;
+        }
+        int j=0;
+        for(int i=minn;i<=maxx;++i){
+            if(mp[i]!=0){
+                while(mp[i]>0){
+                    nums[j++]=i;
+                    mp[i]--;
+                    
+                }
+            }
+        }
         return nums;
     }
 };
