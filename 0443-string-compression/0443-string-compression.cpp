34253f4cc8d12ce8068a493a0e6a912ec8f66dea
AYUSH DIXIT <72985069+ayushD123@users.noreply.github.com>
2023-03-02 21:36:29 +0530

    Time: 3 ms (92.73%), Space: 9 MB (42.12%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -1,46 +1,20 @@
 class Solution {
 public:
     int compress(vector<char>& chars) {
-        // unordered_map<char,int> mp;
-        // for(auto it:chars){
-        //     mp[it]++;
-        // }
-        if(chars.size()<2){
-            return chars.size();
-        }
-       int i=0;
-        int j=1;
-        for(i=0,j=0;j<chars.size();){
-            int cnt=0;
-           // cout<<i<<" "<<j<<endl;
-            while(j<chars.size() && chars[i]==chars[j]){
-                cnt++;
-                j++;
+        int i = 0, res = 0;
+        while (i < chars.size()) {
+            int groupLength = 1;
+            while (i + groupLength < chars.size() && chars[i + groupLength] == chars[i]) {
+                groupLength++;
             }
-            
-            //cout<<cnt<<" ";
-            string c=to_string(cnt);
-            char cn=c[0];
-           // cout<<c<<" ";
-              if(cnt>1){
-                chars.erase(chars.begin()+i,chars.begin()+j-1);
-            }
-             string s=to_string(cnt);
-            if(cnt>1){
-               
-                
-                for(int k=0;k<s.length();++k){
-                    chars.insert(chars.begin()+i+1+k,s[k]);
+            chars[res++] = chars[i];
+            if (groupLength > 1) {
+                for (char c : to_string(groupLength)) {
+                    chars[res++] = c;
                 }
             }
-          // cout<<cnt<<" ";
-           // cout<<s<<endl;
-            if(cnt>1)
-          j=i+s.length()+1;
-           
-            i=j;
+            i += groupLength;
         }
-        return chars.size();
-        
+        return res;
     }
 };
