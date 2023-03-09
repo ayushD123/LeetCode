730d40f92bebd60b2baa58cd1e18e10f39511b9d
AYUSH DIXIT <72985069+ayushD123@users.noreply.github.com>
2023-03-10 00:17:05 +0530

    Time: 4 ms (51.23%), Space: 11.6 MB (49.45%) - LeetHub

--- 2446-determine-if-two-events-have-conflict/2446-determine-if-two-events-have-conflict.cpp
+++ 2446-determine-if-two-events-have-conflict/2446-determine-if-two-events-have-conflict.cpp
@@ -1,40 +1,20 @@
-class Solution {
+class Solution
+{
 public:
-    bool haveConflict(vector<string>& event1, vector<string>& event2) {
-        string ls="";
-        for(auto it:event1[0]){
-            if(it!=':')
-            ls.push_back(it);
-        }
-        int low=stoi(ls);
-        string hs="";
-         for(auto it:event1[1]){
-              if(it!=':')
-            hs.push_back(it);
-        }
-           int high=stoi(hs);
-        string check="";
-        for(auto it:event2[0]){
-             if(it!=':')
-            check.push_back(it);
-        }
-        int ch=stoi(check);
-         string check2="";
-        for(auto it:event2[1]){
-             if(it!=':')
-            check2.push_back(it);
-        }
-        int ch2=stoi(check2);
-         if(ch2>=low && ch2<=high)
-        return 1;
-        if(low>=ch && low<=ch2)
-        return 1;
-        if(high>=ch && high<=ch2)
-        return 1;
-      //  cout<<low<<" "<<high<<" "<<ch2;
-      //  cout<<low<<" "<<high<<" "<<check;
-        if(ch>=low && ch<=high)
-        return 1;
-        return 0;
+    bool haveConflict(vector<string> &event1, vector<string> &event2)
+    {
+        auto x = event1[0], y = event1[1];
+        int a1 = ((x[0] - '0') * 10 + x[1] - '0') * 60 + ((x[3] - '0') * 10 + x[4] - '0');
+        int b1 = ((y[0] - '0') * 10 + y[1] - '0') * 60 + ((y[3] - '0') * 10 + y[4] - '0');
+        x = event2[0], y = event2[1];
+
+        int a2 = ((x[0] - '0') * 10 + x[1] - '0') * 60 + ((x[3] - '0') * 10 + x[4] - '0');
+        int b2 = ((y[0] - '0') * 10 + y[1] - '0') * 60 + ((y[3] - '0') * 10 + y[4] - '0');
+		
+        if (a1 <= a2 && a2 <= b1)
+            return true;
+        if (a2 <= a1 && a1 <= b2)
+            return true;
+        return false;
     }
 };
