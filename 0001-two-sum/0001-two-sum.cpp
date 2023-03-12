2768f106e95151d944604bde733edd3c7e2972ae
AYUSH DIXIT <72985069+ayushD123@users.noreply.github.com>
2023-03-12 23:52:54 +0530

    Time: 19 ms (58.82%), Space: 11.5 MB (11.63%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -4,10 +4,10 @@ public:
         unordered_map<int,int> freq;
         for(int i=0;i<nums.size();++i){
              int x=target-nums[i];
-            if(freq.find(x)!=freq.end()){
-                return {freq[x],i};
+            if(freq[x]!=0){
+                return {freq[x]-1,i};
             }
-            freq[nums[i]]=i;
+            freq[nums[i]]=i+1;
            
         }
         return {0,99};
