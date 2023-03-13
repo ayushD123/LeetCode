c5f7190c4ebc51c287ca7f25e38c4cabd98707d3
AYUSH DIXIT <72985069+ayushD123@users.noreply.github.com>
2023-03-13 23:12:06 +0530

    Time: 737 ms (21.93%), Space: 52.1 MB (11.65%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        if(nums.size()<=1){
-            return nums.size();
-        }
-        int cnt=0;
+    unordered_map<int,bool> mp;
+    unordered_map<int,bool> vis;
+    for(auto it:nums){
+        mp[it]=1;
+    }
         int ans=0;
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size()-1;++i){
-            if(nums[i]==(nums[i+1]-1)){
-               // cout<<"hi";
-             cnt++;   
-            }else if(nums[i]==nums[i+1]){
-            
-                continue;
-            }else{
-               //cout<<ans<<" "<<cnt;
-               ans=max(ans,cnt);
-                cnt=0;
-                
-            }
-            
+    for(auto it:nums){
+        if(mp[it-1]) continue;
+        int x=it;
+        int cnt=0;
+        while(mp[x]){
+            cnt++;
+            x++;
         }
         ans=max(ans,cnt);
+    }
+        return ans;
         
-        return ans+1;
     }
 };
