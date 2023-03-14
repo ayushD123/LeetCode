b6cc142e4216c17dbaca62d614f2a5eb55839ef4
AYUSH DIXIT <72985069+ayushD123@users.noreply.github.com>
2023-03-15 00:55:56 +0530

    Time: 110 ms (38.81%), Space: 47.1 MB (21.94%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
   //  int pref[1e4+10];
-    int subarraySum(vector<int>& nums, int k) {
-        unordered_map<int,int> map;
-        int n=nums.size();
-        int curr=0;
-        int cnt=0;
-        for(int i=0;i<nums.size();++i){
-            curr+=nums[i];
-            
-            if(curr==k){
-                cnt++;
+    int subarraySum(vector<int>& Arr, int K) {
+        int ans=0;
+        int N=Arr.size();
+        unordered_map<int,int> mp;
+        int pre=0;
+        for(int i=0;i<N;++i){
+            pre+=Arr[i];
+            if(pre==K){
+                ans++;
+                
+                
             }
-            if(map.find(curr-k)!=map.end()){
-                cnt+=map[curr-k];
-            }
-            map[curr]++;
+          //  if(mp.find(pre-K)!=mp.end()){
+                ans+=mp[pre-K];
+            //    mp[pre]++;
+           // }else{
+                mp[pre]++;
+           // }
         }
-        cout<<map.size();
-        if(map.size()==1 && n>1){
-            return n*(n+1)/2;
-        }
-        return cnt;
+        return ans;
     }
 };
