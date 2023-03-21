d05e10c0924ddb7b28006f53ad5b17c046c9cdf2
AYUSH DIXIT <72985069+ayushD123@users.noreply.github.com>
2023-03-21 20:30:18 +0530

    Time: 317 ms (5.13%), Space: 133.6 MB (6.34%) - LeetHub

---
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    long long zeroFilledSubarray(vector<int>& nums) {
+        for(int i=0;i<nums.size();++i){
+            if(nums[i]<0){
+                nums[i]*=-1;
+                
+            }
+        }
+        long long ans=0;
+        long long pre=0;
+        unordered_map<long long,int> mp;
+        for(auto it:nums){
+            pre+=it;
+            if(pre==0){
+                ans++;
+            }
+            ans+=mp[pre];
+            mp[pre]++;
+        }
+        return ans;
+    }
+};
