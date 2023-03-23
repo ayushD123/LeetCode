be97a7cd418f205296b1c8726c24ce542bb2088b
AYUSH DIXIT <72985069+ayushD123@users.noreply.github.com>
2023-03-23 17:24:51 +0530

    Added solution - LeetHub

--- Longest subarray with sum divisible by K - GFG/longest-subarray-with-sum-divisible-by-k.cpp
+++ Longest subarray with sum divisible by K - GFG/longest-subarray-with-sum-divisible-by-k.cpp
@@ -10,17 +10,18 @@ public:
 	int longSubarrWthSumDivByK(int arr[], int n, int k)
 	{
 	    // Complete the function
-	    int rem=0;
+	    //int rem=0;
 	    int ans=0;
+	    int pre=0;
 	    unordered_map<int,int> mp;
 	    mp[0]=-1;
 	    for(int i=0;i<n;++i){
-	        rem+=arr[i];
+	        pre+=arr[i];
 	       // if(pre%k==0){
 	       //     ans=max(ans,i+1);
 	            
 	       // }
-	        rem=((rem%k)+k)%k;
+	       int rem=((pre%k)+k)%k;
 	        if(mp.find(rem)!=mp.end()){
 	            int j=mp[rem];
 	            ans=max(ans,i-j);
