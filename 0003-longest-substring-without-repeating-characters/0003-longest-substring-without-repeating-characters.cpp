d4c056c301bf52abed6b34cbf860057936f07050
AYUSH DIXIT <72985069+ayushD123@users.noreply.github.com>
2023-03-23 20:22:16 +0530

    Time: 17 ms (69.56%), Space: 8.8 MB (44.69%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -5,15 +5,11 @@ public:
         int left=0;
         int winsize=0;
         int ans=0;
-        for(int right=0;left<s.length() && right<s.length();){
+        for(int right=0;right<s.length();){
             mp[s[right]]++;
             right++;
              winsize=right-left;
-            
-            if(winsize==mp.size()){
-               ans=max(ans,winsize); 
-            }else{
-                 while(left<s.length() && winsize>0 && mp.size()!=winsize){
+            while(left<=right && mp.size()!=winsize){
                 mp[s[left]]--;
                 if(mp[s[left]]==0){
                     mp.erase(s[left]);
@@ -21,9 +17,12 @@ public:
                 left++;
                      winsize=right-left;
             }
+            if(mp.size()==winsize){
+                ans=max(ans,winsize);
+            }
             }
             
-        }
+        
         return ans;
     }
 };
