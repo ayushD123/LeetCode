b0a879f36e68b22b388d628b7a55c17de19390d8
AYUSH DIXIT <72985069+ayushD123@users.noreply.github.com>
2023-03-25 23:25:51 +0530

    Time: 339 ms (27.47%), Space: 64.3 MB (99.56%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -2,39 +2,53 @@ class Solution {
 public:
     long long maximumSubarraySum(vector<int>& nums, int k) {
         // unordered_set<int> st;
-        unordered_map<int,int> mp;
+      //  unordered_map<int,int> mp;
+        int arr[100010]={0};
         int i=0;
         long long sum=0;
+        int dist=0;
+        if(k==1){
+            return *max_element(nums.begin(),nums.end());
+        }
         while(i<k){
             // st.insert(nums[i]);
-            mp[nums[i]]++;
+           // mp[nums[i]]++;
+            arr[nums[i]]++;
+            if(arr[nums[i]]==1){
+                dist++;
+            }
             sum+=nums[i];
             i++;
         }
         long long ans=0;
-         if(mp.size()==k){
+         if(dist==k){
                 ans=max(ans,sum);
             }
          // for(auto it:st){
          //        cout<<it<<" ";
          //    }
          //    cout<<endl;
-        
+        cout<<dist<<endl;
         for(i=k;i<nums.size();++i){
            
             sum-=nums[i-k];
-            mp[nums[i-k]]--;
+            arr[nums[i-k]]--;
+             if(arr[nums[i-k]]==0)
+                dist--;
             sum+=nums[i];
-            mp[nums[i]]++;
+            arr[nums[i]]++;
             
-            if(mp[nums[i-k]]==0)
-            mp.erase(nums[i-k]);
+            
+             if(arr[nums[i]]==1)
+                dist++;
+           
+           cout<<dist<<endl;
           // // cout<<sum<<" "<<st.size()<<endl;
           //   for(auto it:st){
           //       cout<<it<<" ";
           //   }
           //  cout<<endl;
-             if(mp.size()==k){
+             if(dist==k){
                 ans=max(ans,sum);
             }
         }
