385b31f4991af0ccada416da75e6bb9d417468ff
AYUSH DIXIT <72985069+ayushD123@users.noreply.github.com>
2023-03-26 22:42:09 +0530

    Added solution - LeetHub

---
+++ Count the number of subarrays - GFG/count-the-number-of-subarrays.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution {
+  public:
+  
+long long atmost(vector<int> &v,long long x,int n){
+    int left=0;
+    long long sum=0;
+    int ans=0;
+    long long cnt=0;
+    int right=0;
+  
+    while(left<n){
+        while(right<n && (sum+v[right])<=x){
+            sum+=v[right];
+            right++;
+            cnt+=right-left;
+        }
+        sum-=v[left];
+        left++;
+    }
+    return cnt;
+}
+    long long countSubarray(int n,vector<int> v,long long l,long long r) {
+      long long ans1=atmost(v,r,n);
+   long long ans2=atmost(v,l-1,n);
+   return (ans1-ans2);
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        long long L,R;
+        cin>>N>>L>>R;
+        vector<int> A(N);
+        for(int i=0;i<N;i++){
+            cin>>A[i];
+        }
+        Solution obj;
+        auto ans = obj.countSubarray(N,A,L,R);
+        cout<<ans<<endl;
+    }
+}  
+// } Driver Code Ends
