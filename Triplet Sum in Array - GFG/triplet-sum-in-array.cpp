cf35f94426f62de78ff6e96d63c9b10909a9929e
AYUSH DIXIT <72985069+ayushD123@users.noreply.github.com>
2023-03-28 21:17:34 +0530

    Added solution - LeetHub

--- Triplet Sum in Array - GFG/triplet-sum-in-array.cpp
+++ Triplet Sum in Array - GFG/triplet-sum-in-array.cpp
@@ -10,19 +10,37 @@ class Solution{
     //array A[] which sums up to X.
     bool find3Numbers(int arr[], int n, int X)
     {
-        for(int i=0;i<n;++i){
-             unordered_map<int,int> mp;
-             int two_sum=X-arr[i];
-             for(int j=i+1;j<n;++j){
-                 if(mp.find(two_sum-arr[j])!=mp.end()){
-                     return 1;
-                 }
-                 mp[arr[j]]++;
+        // for(int i=0;i<n;++i){
+        //      unordered_map<int,int> mp;
+        //      int two_sum=X-arr[i];
+        //      //two sum probelm
+        //      for(int j=i+1;j<n;++j){
+        //          if(mp.find(two_sum-arr[j])!=mp.end()){
+        //              return 1;
+        //          }
+        //          mp[arr[j]]++;
                  
-             }
+        //      }
+        // }
+        // return 0;
+        sort(arr,arr+n);
+        for(int i=0;i<n-2;++i){
+            int k=X-arr[i];
+            int left=i+1;
+            int right=n-1;
+            while(left<right){
+                int sum=arr[left]+arr[right];
+                if(sum==k){
+                    return 1;
+                }
+                if(sum<k){
+                    left++;
+                }else{
+                    right--;
+                }
+            }
         }
-        return 0;
-      
+      return 0;
        
     }
 
