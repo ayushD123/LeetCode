631232148ba56f06ec41a11868842cbd8c1e85a7
AYUSH DIXIT <72985069+ayushD123@users.noreply.github.com>
2023-04-14 23:03:48 +0530

    Time: 27 ms (99.76%), Space: 19.1 MB (11.87%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,38 +1,28 @@
 class Solution {
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
-        int low=0;
+        int low=1;
         int high=*max_element(piles.begin(),piles.end());
-        int ans=1e9;
-        if(piles.size()==1){
-            if(piles[0]%h==0){
-                return piles[0]/h;
-            }
-            return (piles[0]/h + 1);
-        }
+        int ans=0;
         while(low<=high){
-            int mid=low+(high-low)/2;
-            if(mid==0) break;
-            int sum=0;
-            int k=0;
-            int rem=0;
+            int mid=(low+high)/2;
+            long long cnt=0;
             for(auto it:piles){
-                 
-               if(it%mid==0){
-                 k+=it/mid;  
-               }else{
-                   k+=((it/mid)+1);
-               }
-                  
+                if(it%mid==0){
+                    cnt+=(it/mid);
+                }else{
+                    int x=(it/mid)+1;
+                     cnt+=x;
+                }
+                
+             //  cout<<cnt<<endl;
             }
-           // cout<<mid<<" "<<k<<endl;
-            if(k<=h){
+            if(cnt<=h){
+                ans=mid;
                 high=mid-1;
-                ans=min(ans,mid);
             }else{
                 low=mid+1;
             }
-            
         }
         return ans;
     }
