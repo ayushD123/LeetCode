0afa738e86b389fb7147f05c643e0053f8db8b49
AYUSH DIXIT <72985069+ayushD123@users.noreply.github.com>
2023-04-16 00:47:10 +0530

    Time: 172 ms (73.13%), Space: 57.2 MB (31.72%) - LeetHub

---
+++ 2560-house-robber-iv/2560-house-robber-iv.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minCapability(vector<int>& nums, int k) {
+        int low=*min_element(nums.begin(),nums.end());
+         int high=*max_element(nums.begin(),nums.end());
+        int ans=INT_MAX;
+        while(low<=high){
+            int mid=(low+high)/2;
+            int cnt=0;
+            for(int i=0;i<nums.size();++i){
+                if(nums[i]<=mid){
+                    cnt++;
+                    i++;
+                }
+            }
+            if(cnt>=k){
+                ans=min(ans,mid);
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+};
