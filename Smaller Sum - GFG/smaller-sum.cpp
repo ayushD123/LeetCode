10632598673c59db1686f6b9dc77889fccc20baf
AYUSH DIXIT <72985069+ayushD123@users.noreply.github.com>
2023-04-22 21:55:25 +0530

    Added solution - LeetHub

---
+++ Smaller Sum - GFG/smaller-sum.cpp
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution{
+public:
+    vector<long long> smallerSum(int n,vector<int> &arr){
+        // Code here
+        vector<long long > ans;
+        unordered_map<int,long long> mp;
+        vector<int> tmp=arr;
+        sort(tmp.begin(),tmp.end());
+        vector<long long > pre(n,0);
+        pre[0]=0;
+        for(int i=1;i<n;++i){
+            pre[i]=pre[i-1]+tmp[i-1];
+        }
+        
+        mp[tmp[0]]=0;
+        for(int i=1;i<n;++i){
+            if(tmp[i]==tmp[i-1]){
+                continue;
+            }
+            mp[tmp[i]]=pre[i];
+        }
+        for(auto it:arr){
+            ans.push_back(mp[it]);
+        }
+        return ans;
+    }
+    
+};
+
+//{ Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> arr(n);
+        for(int i=0;i<n;i++){
+            cin>>arr[i];
+        }
+        Solution ob; 
+        vector<long long> ans=ob.smallerSum(n,arr);
+        for(int i=0;i<n;i++){
+            if(i!=n-1){
+                cout<<ans[i]<<" ";
+            }
+            else{
+                cout<<ans[i]<<endl;
+            }
+        }
+    }
+    return 0;
+}
+// } Driver Code Ends
