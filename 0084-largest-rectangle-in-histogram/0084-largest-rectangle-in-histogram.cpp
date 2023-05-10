c42259f948486f7de34ce357646542c085a31c37
AYUSH DIXIT <72985069+ayushD123@users.noreply.github.com>
2023-05-10 19:48:06 +0530

    Time: 216 ms (46.87%), Space: 81.5 MB (54.85%) - LeetHub

---
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        
+        stack<int> str;
+        stack<int> stl;
+        int n=heights.size();
+        int mn=*min_element(heights.begin(),heights.end());
+        vector<int> l(n,-1);
+        vector<int> r(n,-1);
+        for(int i=n-1;i>=0;i--){
+            while(!str.empty() && heights[str.top()]>=heights[i]){
+                str.pop();
+            }
+            if(!str.empty()){
+                r[i]=str.top();
+            }
+            str.push(i);
+            
+        }
+        for(int i=0;i<n;i++){
+            while(!stl.empty() && heights[stl.top()]>=heights[i]){
+                stl.pop();
+            }
+            if(!stl.empty()){
+                l[i]=stl.top();
+            }
+            stl.push(i);
+            
+        }
+             int ans=n*mn;
+        for(int i=0;i<n;++i){
+            
+                 if(l[i]==(-1)){
+                l[i]=-1;
+            }
+             if(r[i]==(-1)){
+                r[i]=n;
+            }
+            
+            int h=heights[i]*(r[i]-l[i]-1);
+            ans=max(ans,h);
+           // cout<<h<<" "<<i<<" "<<l[i]<<" "<<r[i]<<endl;
+        }
+       
+       return ans;
+    }
+};
