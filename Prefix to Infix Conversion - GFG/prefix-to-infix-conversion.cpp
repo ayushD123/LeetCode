39062f72ac47f59eab0e76d6d29a0fe63bf22333
AYUSH DIXIT <72985069+ayushD123@users.noreply.github.com>
2023-05-13 01:31:51 +0530

    Added solution - LeetHub

---
+++ Prefix to Infix Conversion - GFG/prefix-to-infix-conversion.cpp
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    string preToInfix(string str) {
+        // Write your code here
+        stack<string> st;
+        for(int i=str.length()-1;i>=0;i--){
+            char c=str[i];
+            if((c>='A' && c<='Z') ||(c>='a' && c<='z')){
+                string tmp="";
+                tmp+=c;
+                st.push(tmp);
+            }else{
+                string a='('+st.top();
+                st.pop();
+                a+=c;
+                a+=st.top()+')';
+                st.pop();
+                st.push(a);
+            }
+        }
+        return st.top();
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t = 1;
+    cin >> t;
+
+    // freopen ("output_gfg.txt", "w", stdout);
+
+    while (t--) {
+        // Input
+        string prefix;
+        cin >> prefix;
+
+        Solution obj;
+        cout << obj.preToInfix(prefix) << endl;
+
+        // cout << "~\n";
+    }
+    // fclose(stdout);
+
+    return 0;
+}
+
+// } Driver Code Ends
