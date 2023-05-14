6934cb26107dbfb447f4a7030c94b3bca038c5bb
AYUSH DIXIT <72985069+ayushD123@users.noreply.github.com>
2023-05-15 00:38:44 +0530

    Time: 22 ms (16.19%), Space: 8.9 MB (38.82%) - LeetHub

---
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string removeKdigits(string num, int k) {
+        stack<char> st;
+        for(auto it:num){
+            while(!st.empty() && k>0 && (st.top()>it)){
+                 //cout<<st.top()<<" ";
+                st.pop();
+               
+                k--;
+            }
+            st.push(it);
+         //   if(k<=0) break;
+        }
+        while(!st.empty() && k>0){
+            st.pop();
+            k--;
+        }
+        if(st.empty()) return "0";
+        string ans="";
+        
+        while(!st.empty()){
+            ans+=st.top();
+            st.pop();
+        };
+       // cout<<ans.back();
+        while(ans.back()=='0' && ans.length()>1) ans.pop_back();
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
