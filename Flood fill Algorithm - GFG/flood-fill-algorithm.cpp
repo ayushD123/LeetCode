3e98dfd00fbb0327aee7955cdb6341fedb6d73a8
AYUSH DIXIT <72985069+ayushD123@users.noreply.github.com>
2023-05-18 20:43:27 +0530

    Added solution - LeetHub

--- Flood fill Algorithm - GFG/flood-fill-algorithm.cpp
+++ Flood fill Algorithm - GFG/flood-fill-algorithm.cpp
@@ -18,6 +18,26 @@ private:
         dfs(image,sr,sc+1,newColor,oldcolor,vis);
         dfs(image,sr,sc-1,newColor,oldcolor,vis);
     }
+    void dfs1(vector<vector<int>>& image, int sr, int sc, int newColor,int oldcolor,vector<vector<int>>& vis){
+       // vis[sr][sc]=1;
+       int n=image.size();
+       int m=image[0].size();
+    vis[sr][sc]=1;
+    image[sr][sc]=newColor;
+    for(int del_row=-1;del_row<=1;del_row++){
+                for(int del_col=-1;del_col<=1;del_col++){
+		    //not to check the diagonal elements.
+                    if((del_row==-1 && del_col==-1) || (del_row==-1 && del_col==1) || (del_row==1 && del_col==-1) || (del_row==1 && del_col==1)){
+                        continue;
+                    }
+                    int nrow=sr+del_row;
+                    int ncol=sc+del_col;
+                    if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && image[nrow][ncol]==oldcolor && vis[nrow][ncol]==0){
+                        dfs1(image,nrow,ncol,newColor,oldcolor,vis);
+                    }
+                }
+    }
+    }
 
 public:
 
@@ -28,7 +48,7 @@ public:
         int m=image[0].size();
          vector<vector<int> > vis(n,vector<int>(m,0));
          int initial=image[sr][sc];
-        dfs(image,sr,sc,newColor,initial,vis);
+        dfs1(image,sr,sc,newColor,initial,vis);
         return image;
     }
 };
