5e413f354d6da85bdf7cafaeae9dc6f951dfc1e8
AYUSH DIXIT <72985069+ayushD123@users.noreply.github.com>
2023-05-20 21:07:56 +0530

    Time: 1282 ms (5.07%), Space: 134.6 MB (76.70%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -5,11 +5,12 @@ public:
         vector<int> ans;
         for(int i=0;i<nums.size();++i){
             if(!dq.empty() && dq.front()==(i-k)) dq.pop_front();
-            while(!dq.empty() && nums[dq.back()]<nums[i]){
+            while(!dq.empty() && nums[dq.back()]<=nums[i]){
                 dq.pop_back();
             }
             dq.push_back(i);
             if(i>=k-1) ans.push_back(nums[dq.front()]);
+            cout<<dq.front()<<endl;
         }
         return ans;
     }
