32ac295fd78138e98b8bde26c80244f011c14b73
AYUSH DIXIT <72985069+ayushD123@users.noreply.github.com>
2023-07-22 02:24:14 +0530

    Time: 3 ms (35.75%), Space: 5.9 MB (89.76%) - LeetHub

---
+++ 0461-hamming-distance/0461-hamming-distance.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int cnt=0;
+        for(int i=31;i>=0;i--){
+            int a=(x&1);
+            int b=(y&1);
+            if(a!=b) cnt++;
+            x=x>>1;
+            y=y>>1;
+        }
+        return cnt;
+    }
+};
