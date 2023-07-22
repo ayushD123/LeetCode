f1df49becd2da522ea0ceca78bce7669ec78c313
AYUSH DIXIT <72985069+ayushD123@users.noreply.github.com>
2023-07-23 00:38:00 +0530

    Time: 7 ms (91.34%), Space: 9.9 MB (58.89%) - LeetHub

---
+++ 0260-single-number-iii/0260-single-number-iii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        int xorres=0;
+        vector<int> ans;
+        for(auto it:nums){
+            xorres^=it;
+        }
+        int pos=0;
+            int i=0;
+        int temp=xorres;
+        while(1){
+            if(temp&1){
+                pos=i;
+                break;
+            }
+            i++;
+            temp=temp>>1;
+        }
+        temp=0;
+        for(auto it:nums){
+            if(it&(1<<pos)){
+                temp^=it;
+            }
+        }
+        
+        ans.push_back(temp);
+        ans.push_back(xorres^temp);
+        return ans;
+    }
+};
