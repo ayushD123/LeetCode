f4fb0808fdaea8f4367e635e17a285ec247fff37
AYUSH DIXIT <72985069+ayushD123@users.noreply.github.com>
2023-08-14 01:41:18 +0530

    Time: 31 ms (31.22%), Space: 15 MB (47.79%) - LeetHub

---
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool dfs(map<string,vector<string>>& mp, string city, vector<string>& ans, int n) {
+        if (ans.size() == n) return true;
+        if (mp[city].size() == 0) return false;
+            
+        auto& destinations = mp[city];
+        for (int i = 0; i < destinations.size(); ++i) {
+            string dest = destinations[i];
+            ans.push_back(dest);
+            destinations.erase(destinations.begin() + i);
+            if (dfs(mp, dest, ans, n)) return true;
+            ans.pop_back();
+            destinations.insert(destinations.begin() + i, dest); // Insert back at the correct position
+        }
+        return false;
+    }
+    
+    vector<string> findItinerary(vector<vector<string>>& tickets) {
+        sort(tickets.begin(), tickets.end());
+        map<string,vector<string>> mp;
+        for (auto it : tickets) {
+            mp[it[0]].push_back(it[1]);
+        }
+        vector<string> ans;
+        ans.push_back("JFK");
+        dfs(mp, "JFK", ans, tickets.size() + 1);
+        return ans;
+    }
+};
