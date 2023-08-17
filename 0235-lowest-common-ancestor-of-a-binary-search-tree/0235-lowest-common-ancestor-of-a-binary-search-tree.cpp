dda18f24f5272c93f19998d0dbe9e865c5724dd9
AYUSH DIXIT <72985069+ayushD123@users.noreply.github.com>
2023-08-17 23:58:08 +0530

    Time: 66 ms (5.19%), Space: 37.7 MB (16.47%) - LeetHub

---
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+     void dfs(TreeNode* curr, TreeNode* parn, unordered_map<TreeNode*, int>& depth, unordered_map<TreeNode*, TreeNode*>& par) {
+        if (curr == NULL) return;
+        par[curr] = parn;
+        depth[curr] = depth[parn] + 1;
+        dfs(curr->left, curr, depth, par);
+        dfs(curr->right, curr, depth, par);
+    }
+    
+    TreeNode* lca(TreeNode* p, TreeNode* q, unordered_map<TreeNode*, int>& depth, unordered_map<TreeNode*, TreeNode*>& par) {
+        if (depth[p] < depth[q]) swap(p, q);
+        int diff = depth[p] - depth[q];
+        while (diff--) {
+            p = par[p];
+        }
+        while (p->val != q->val) {
+            p = par[p];
+            q = par[q];
+        }
+        return p;
+    }
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+ unordered_map<TreeNode*, int> depth;
+        unordered_map<TreeNode*, TreeNode*> par;
+        depth[NULL] = 0; // Set depth of NULL to -1
+        dfs(root, NULL, depth, par);
+        return lca(p, q, depth, par);
+        
+    }
+};
