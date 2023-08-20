5916fce241054572a0ec35ef77f6827120ab16ac
AYUSH DIXIT <72985069+ayushD123@users.noreply.github.com>
2023-08-21 01:20:10 +0530

    Time: 7 ms (82.36%), Space: 20.3 MB (40.36%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.cpp
@@ -11,24 +11,39 @@
  */
 class Solution {
 public:
-    int maxd=0;
-    int height(TreeNode* node){
-        if(node==NULL) return 0;
-        int l=height(node->left);
-        int r=height(node->right);
+    //O(Nsq)
+//     int maxd=0;
+//     int height(TreeNode* node){
+//         if(node==NULL) return 0;
+//         int l=height(node->left);
+//         int r=height(node->right);
+//         return 1+max(l,r);
+//     }
+    
+//     void diam(TreeNode* node){
+//         if(node==NULL) return;
+//         int lh=height(node->left);
+//         int rh=height(node->right);
+//         maxd=max(maxd,lh+rh);
+//         diam(node->left);
+//         diam(node->right);
+//     }
+//     int diameterOfBinaryTree(TreeNode* root) {
+//         diam(root);
+//         return maxd;
+//     }
+    int heigh(TreeNode* node,int& diam){
+         if(node==NULL) return 0;
+        int l=heigh(node->left,diam);
+        int r=heigh(node->right,diam);
+        diam=max(diam,l+r);
         return 1+max(l,r);
     }
+      int diameterOfBinaryTree(TreeNode* root) {
+          int diam=0;
+        heigh(root,diam);
+        return diam;
+    }
     
-    void diam(TreeNode* node){
-        if(node==NULL) return;
-        int lh=height(node->left);
-        int rh=height(node->right);
-        maxd=max(maxd,lh+rh);
-        diam(node->left);
-        diam(node->right);
-    }
-    int diameterOfBinaryTree(TreeNode* root) {
-        diam(root);
-        return maxd;
-    }
+    
 };
