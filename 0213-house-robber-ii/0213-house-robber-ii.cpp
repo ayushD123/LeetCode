586da62e3eeff59732e2e16a180eb65e40af303d
AYUSH DIXIT <72985069+ayushD123@users.noreply.github.com>
2023-09-18 00:03:08 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (5.80%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,3 +1,42 @@
+// class Solution {
+// public:
+//     int space(vector<int> &nums){
+//         int n=nums.size(); //n-2
+//         int a=nums[0];  //n-1
+//         int b=max(nums[0],nums[1]);
+//         for(int i=2;i<n;++i){
+//             int pick=nums[i]+a;
+//             int npick=0+b;
+//             int curr=max(pick,npick);
+//             a=b;
+//             b=curr;
+//         } 
+        
+//         return b;
+//     }
+ 
+        
+       
+        
+//     int rob(vector<int>& nums) {
+//         if(nums.size()==1){
+//             return nums[0];
+//         }
+//         vector<int> arr1;
+//          vector<int> arr2;
+//         for(int i=1;i<nums.size();++i){
+//             arr1.push_back(nums[i]);
+//         }
+//           for(int i=0;i<nums.size()-1;++i){
+//             arr2.push_back(nums[i]);
+//         }
+//         int ans1= space(arr1);
+        
+//         int ans2= space(arr2);
+//         return max(ans1,ans2);
+//     }
+// };
+
 class Solution {
 public:
     int space(vector<int> &nums){
