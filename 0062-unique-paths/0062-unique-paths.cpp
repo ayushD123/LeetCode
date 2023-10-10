7fae6d87db634634dc5bf51b8e1ee95012fc4a25
AYUSH DIXIT <72985069+ayushD123@users.noreply.github.com>
2023-10-10 22:35:44 +0530

    Time: 0 ms (100.00%), Space: 6.8 MB (6.81%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,14 +1,41 @@
 class Solution {
 public:
-    int solve(int m,int n,  vector<vector<int>> &dp){
-        if(m==0 && n==0){
-            return 1;
+    //memo
+    // int solve(int m,int n,  vector<vector<int>> &dp){
+    //     if(m==0 && n==0){
+    //         return 1;
+    //     }
+    //     if(m<0 || n<0) return 0;
+    //     if(dp[m][n]!=(-1)) return dp[m][n];
+    //     int left=solve(m-1,n,dp);
+    //     int top=solve(m,n-1,dp);
+    //     return dp[m][n]=left+top;
+    // }
+    /*
+        steps to convert memo into tab-
+            1)write base case inside dp array
+            2)write all possible states through for loops
+            3)copy recurrence relation and use dp array
+    */
+    //tab
+    int tab(int m,int n,  vector<vector<int>> &dp){
+        dp[0][0]=1; //write base case inside dp array
+        for(int i=0;i<m;++i){
+            for(int j=0;j<n;++j){
+                if(i==0 && j==0) continue;
+                int left=0;
+                int top=0;
+                if(i>0){
+                     left=dp[i-1][j];
+                }
+               if(j>0){
+                top=dp[i][j-1];
+               }
+                
+                dp[i][j]=left+top;
+            }
         }
-        if(m<0 || n<0) return 0;
-        if(dp[m][n]!=(-1)) return dp[m][n];
-        int left=solve(m-1,n,dp);
-        int right=solve(m,n-1,dp);
-        return dp[m][n]=left+right;
+        return dp[m-1][n-1];
     }
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp;
@@ -16,7 +43,8 @@ public:
             vector<int> t(n,-1);
             dp.push_back(t);
         }
-        return solve(m-1,n-1,dp); // m and n given as 1 based
+    //    return solve(m-1,n-1,dp); // m and n given as 1 based
+        return tab(m,n,dp);
         
     }
 };
